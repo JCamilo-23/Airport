30b8e2312505205ae927d9331a219ed2c1a4f2ee
BryanS112 <bssandoval@uninorte.edu.co>
2025-05-21 15:30:49 -0500

    second commit

--- src/core/models/flight/Flight.java
+++ src/core/models/flight/Flight.java
@@ -2,8 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package airport;
+package core.models.flight;
 
+import core.models.person.Passenger;
+import core.models.Plane;
+import core.models.Location;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
